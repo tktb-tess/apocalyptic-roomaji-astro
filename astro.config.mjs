4cb043d787eea63aee39316d3c044dd6165d7147
tktb-tess <chemusiclove6174@gmail.com>
2026-04-08 18:10:50 +0900

    色々

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import remarkDirective from 'remark-directive';
-import { tableHandler, textDirectiveHandler } from './handlers';
+import * as H from './handlers';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
   trailingSlash: 'always',
   vite: {
     plugins: [tailwindcss()],
@@ -17,8 +18,8 @@ export default defineConfig({
     remarkPlugins: [remarkDirective],
     remarkRehype: {
       handlers: {
-        table: tableHandler,
-        textDirective: textDirectiveHandler,
+        table: H.tableHandler,
+        textDirective: H.textDirectiveHandler,
       },
     },
   },
